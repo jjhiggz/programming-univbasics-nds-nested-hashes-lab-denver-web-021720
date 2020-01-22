db1dc4132a63b079725802a229ee9c4f41c2c92d
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-22 12:04:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,10 +15,11 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-    array = [DON_G,JOELLE_VD,PAT_M,KATE_G,BRUCE_G]
+  return  array = [DON_G,JOELLE_VD,PAT_M,KATE_G,BRUCE_G]
 end
 
 def aoh_lookup(aoh, row, key)
+  return
 end
 
 def aoh_update(aoh, row, key, new_value)
